fcd94f31b6a3a2effb35260a8cc803a5ae33b190
shubham567 <shubhamshekhar567@gmail.com>
2022-06-19 20:14:33 +0530

    added redux store

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button, Modal, ActivityIndicator } from 'react-native';
 import {useEffect, useState} from "react";
 import * as Network from "expo-network";
+import {Provider} from "react-redux";
+import store from "./src/store/store";
 
 
 export default function App() {
@@ -13,44 +15,50 @@ export default function App() {
     setIsConnectedToInternet(networkData.isConnected)
   }, []);
 
+  const login = () => {
+
+  }
+
 
   return (
-    <View style={styles.container}>
-      <Text  style={{fontSize: 20}}>Native Demo App</Text>
-      <Text  style={{fontSize: 14}}>This app is built to establish the use of React Native</Text>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text  style={{fontSize: 20}}>Native Demo App</Text>
+        <Text  style={{fontSize: 14}}>This app is built to establish the use of React Native</Text>
 
-      <View style={{margin: 20, marginTop: 50}}>
-        <Text>
-          Username:
-        </Text>
-        <TextInput placeholder="Enter username :"/>
-      </View>
-      <View style={{margin: 10}}>
-        <Text>
-          Password:
-        </Text>
-        <TextInput placeholder="Enter Password :"/>
-      </View>
-      <View>
-        <Button title="Login"/>
-      </View>
-      <StatusBar style="auto" />
-      <Modal animationType="slide"
-             transparent={false}
-             visible={!isConnectedToInternet}
-             onRequestClose={() => setIsConnectedToInternet(false)}
-      >
-        <View style={{height: "100%", paddingTop: 300, paddingLeft: 30}}>
-          {
-            isConnectedToInternet === null &&
-            <ActivityIndicator />
-          }
-          <Text style={{fontSize: 18}}>{
-            isConnectedToInternet === false || isConnectedToInternet === null ? "Not Connected to internet" : "Connected to Internet"
-          }</Text>
+        <View style={{margin: 20, marginTop: 50}}>
+          <Text>
+            Username:
+          </Text>
+          <TextInput placeholder="Enter username :"/>
         </View>
-      </Modal>
-    </View>
+        <View style={{margin: 10}}>
+          <Text>
+            Password:
+          </Text>
+          <TextInput placeholder="Enter Password :"/>
+        </View>
+        <View>
+          <Button title="Login"/>
+        </View>
+        <StatusBar style="auto" />
+        <Modal animationType="slide"
+               transparent={false}
+               visible={!isConnectedToInternet}
+               onRequestClose={() => setIsConnectedToInternet(false)}
+        >
+          <View style={{height: "100%", paddingTop: 300, paddingLeft: 30}}>
+            {
+              isConnectedToInternet === null &&
+              <ActivityIndicator />
+            }
+            <Text style={{fontSize: 18}}>{
+              isConnectedToInternet === false || isConnectedToInternet === null ? "Not Connected to internet" : "Connected to Internet"
+            }</Text>
+          </View>
+        </Modal>
+      </View>
+    </Provider>
   );
 }
 
---
+++ src/store/constants/loginReducerConstants.js
@@ -0,0 +1,6 @@
+const loginReducerConstants = {
+  LOGIN_STATE_CHANGE: "LOGIN_STATE_CHANGE",
+
+}
+
+export default loginReducerConstants;
